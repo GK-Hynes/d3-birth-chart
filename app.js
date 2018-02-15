b122a8ccb870962025d64f5bb3e93ff5b32a1cb9
Gerard <gkhynes@gmail.com>
2018-02-15 21:02:18 +0000

    Handle extrema and scale with D3

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var minYear = birthData[0].year;
-var maxYear = birthData[birthData.length - 1].year;
+var minYear = d3.min(birthData, function(d) {
+  return d.year;
+});
+var maxYear = d3.max(birthData, function(d) {
+  return d.year;
+});
 var width = 600;
 var height = 600;
 var barPadding = 10;
 var numBars = 12;
 var barWidth = width / numBars - barPadding;
+var maxBirths = d3.max(birthData, function(d) {
+  return d.births;
+});
+// prettier-ignore
+var yScale = d3.scaleLinear()
+                .domain([0, maxBirths]) 
+                .range([height, 0]); // flips the y-axis for easier calculations
 
 // prettier-ignore
 d3.select("input")
@@ -24,10 +35,10 @@ d3.select("svg")
   .append("rect")
     .attr("width", barWidth)
     .attr("height", function(d) {
-        return d.births / 2.5e6 * height;
+        return height - yScale(d.births);
     })
     .attr("y", function(d) {
-        return height - d.births / 2.5e6 * height;
+        return yScale(d.births);
     })
     .attr("x", function(d,i) {
         return (barWidth + barPadding) * i;
@@ -43,9 +54,9 @@ d3.select("input")
                 return d.year === year;
             }))
                 .attr("height", function(d) {
-                    return d.births / 2.5e6 * height;
+                    return height - yScale(d.births);
                 })
                 .attr("y", function(d) {
-                    return height - d.births / 2.5e6 * height;
+                    return yScale(d.births);
                 });
     });
